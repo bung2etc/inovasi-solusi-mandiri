a19eb8d075ea7bb9f48f8808755e61a0e38c1c15
bungams <manafbunga99@gmail.com>
2026-06-25 20:06:36 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// ================= SMOOTH SCROLL =================
+
+document
+  .querySelectorAll('a[href^="#"]')
+  .forEach(anchor => {
+
+    anchor.addEventListener('click', function(e) {
+
+      const target = document.querySelector(
+        this.getAttribute('href')
+      );
+
+      if(target) {
+
+        e.preventDefault();
+
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+
+    });
+
+  });
